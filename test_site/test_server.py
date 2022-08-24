8682436c820388d93ba68e2180c73adc2d866ba1
Zomlik <sanek77594@yandex.ru>
2022-08-24 10:01:53 +0300

    rm

--- test_site/test_server.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("",PORT), Handler) as httppd:
-    print("server is on at port", PORT)
-    httppd.serve_forever()
